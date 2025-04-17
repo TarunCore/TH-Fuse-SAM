e4c0be27b213cae43030de8ce5fba98f9c7286f1
Tarun E <112856864+TarunCore@users.noreply.github.com>
2025-04-17 22:50:47 +0530

    args'.py fixes

--- args.py
+++ args.py
@@ -1,4 +1,4 @@
-epochs = 36  
+epochs = 2  
 batch_size = 2  
 image_height = 256 
 image_width = 256
@@ -6,5 +6,5 @@ train_num = 7601
 learning_rate = 1e-4
 learning_rate_d = 1e-4
 log_interval = 10
-dataset_path = "/content/visible-infrared-wildfire-experiment/m300_grabbed_data_1_51.2/m300_grabbed_data_1_51.2/rgb
+dataset_path = "/content/visible-infrared-wildfire-experiment/m300_grabbed_data_1_51.2/m300_grabbed_data_1_51.2/rgb"
 save_model_path = "/content/"
